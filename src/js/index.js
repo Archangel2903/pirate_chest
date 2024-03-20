c3676822363e89d7d5b6ef579f8d375cf162363a
Archangel2903 <archangel2903@gmail.com>
2024-03-20 11:33:33 +0200

    lazzy load delete

--- src/js/index.js
+++ src/js/index.js
@@ -59,23 +59,4 @@ $(function () {
 
         steps++
     });
-
-    // Lazy load observer
-    const imagesAll = document.querySelectorAll('img[data-src]');
-    let imgObserve = new IntersectionObserver(function (entries) {
-        entries.forEach(function (entry) {
-            if (entry.intersectionRatio >= 0 && entry.target.hasAttribute('data-src')) {
-                let current = entry.target;
-                let source = current.getAttribute('data-src');
-
-                current.setAttribute('src', source);
-                current.removeAttribute('data-src');
-            }
-        });
-    });
-    if (imagesAll.length > 0) {
-        imagesAll.forEach(function (image) {
-            imgObserve.observe(image);
-        });
-    }
 });
